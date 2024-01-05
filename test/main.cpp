464a1dd0b1c4dc842806a10cc03c47384d61424a
Timur Akhmetzjanov <ahmetzianov.temur@ya.ru>
2024-01-05 11:20:27 +0300

    Delete test directory

--- test/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "../includes/Dynamic.h"
-
-int main() {
-  std::function<int(int, int)> f = [](int n, int m){ std::cout << n; return m; };
-
-  int n = 1, m = 2;
-  std::cout << f(n, m);
-}
