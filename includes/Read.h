5e78614025d030efb61260ce1e7281d37ea702fe
Тимур Ахметзянов <ahmetzianov.temur@ya.ru>
2024-01-05 13:29:46 +0500

    delete Read.cpp and Write.cpp

--- includes/Read.h
+++ includes/Read.h
@@ -9,13 +9,140 @@
 
 class Read {
  public:
-  virtual Vector3 readVector3(std::istream& in);
-  virtual Vector3 Vector3FromStdString(const std::string& string);
+  virtual Vector3 readVector3(std::istream& in) {
+    double p1, p2, p3;
+    in >> p1 >> p2 >> p3;
+    return {p1, p2, p3};
+  }
 
-  virtual Particle readParticle(std::istream& in);
-  virtual Particle ParticleFromStdString(const std::string& string);
+  virtual Vector3 Vector3FromStdString(const std::string& string) {
+    double param;
+    Vector3 vector3;
+    int cnt = 0;
+    bool is_dot = false;
+    int cnt_dots = 0;
+    for (char let : string) {
+      if (let == ' ') {
+        ++cnt;
+        param /= std::pow(10, cnt_dots);
+        is_dot = false;
+        cnt_dots = 0;
+        if (cnt == 1) {
+          vector3.setP1(param);
+          param = 0;
+        } else if (cnt == 2) {
+          vector3.setP2(param);
+          param = 0;
+        }
+      }
 
-  virtual Dynamic readSystem(std::istream& in, bool read_force, int particles_cnt);
+      if (let == '.') {
+        is_dot = true;
+        continue;
+      }
+
+      if (is_dot) {
+        ++cnt_dots;
+      }
+
+      param = param * 10 + let - '0';
+    }
+    param /= std::pow(10, cnt_dots);
+    vector3.setP3(param);
+    return vector3;
+  }
+
+  virtual Particle readParticle(std::istream& in) {
+    Vector3 vel = readVector3(in);
+    Vector3 coo = readVector3(in);
+    double mass;
+    float I;
+    in >> mass >> I;
+    return {vel, coo, mass, I};
+  }
+
+  virtual Particle ParticleFromStdString(const std::string& string) {
+    std::string part;
+    int cnt_spaces = 0;
+    Vector3 vel, coo;
+    double mass;
+    float I;
+    bool got_mass;
+    int cnt_dots = 0;
+    bool is_dot = false;
+    for (char let : string) {
+      if (let == ' ') {
+        ++cnt_spaces;
+      }
+
+      if (cnt_spaces == 3) {
+        vel = Read().Vector3FromStdString(part);
+        part = "";
+        is_dot = false;
+        cnt_dots = 0;
+        continue;
+      }
+
+      if (cnt_spaces == 6) {
+        coo = Read().Vector3FromStdString(part);
+        part = "";
+        is_dot = false;
+        cnt_dots = 0;
+        continue;
+      }
+
+      if (cnt_spaces == 7) {
+        mass /= std::pow(10, cnt_dots);
+        cnt_dots = 0;
+        is_dot = false;
+        got_mass = true;
+      }
+
+      if (let == '.') {
+        if (cnt_spaces >= 6) {
+          is_dot = true;
+        } else {
+          part.push_back(let);
+        }
+        continue;
+      }
+
+      if (is_dot) {
+        ++cnt_dots;
+      }
+
+      if (cnt_spaces < 6) {
+        part.push_back(let);
+      } else {
+        if (got_mass) {
+          I = I * 10 + float(let - '0');
+        } else {
+          mass = mass * 10 + let - '0';
+        }
+      }
+    }
+    I /= (float)std::pow(10, cnt_dots);
+
+    return {vel, coo, mass, I};
+  }
+
+  virtual Dynamic readSystem(std::istream& in, bool read_force, int particles_cnt) {
+    Vector3 force(0, 0, 0);
+    if (read_force) {
+      force = readVector3(in);
+    }
+
+    std::vector<Particle> particles(particles_cnt);
+    for (int i = 0; i < particles_cnt; ++i) {
+      particles[i] = readParticle(in);
+    }
+    return {force, particles};
+  }
+
   virtual Dynamic readSystem(std::istream& in, bool read_force, int particles_cnt,
-                             const std::function<Vector3(Particle, Particle)>& calculateForce);
+                             const std::function<Vector3(Particle, Particle)>& calculateForce) {
+    Dynamic dyn = readSystem(in, read_force, particles_cnt);
+    dyn.setCalculateForce(calculateForce);
+    return dyn;
+  }
 };
--- includes/Write.h
+++ includes/Write.h
@@ -9,9 +9,29 @@
 
 class Write {
  public:
-  virtual void printVector3(std::ostream& out, const Vector3& vec);
+  virtual void printVector3(std::ostream& out, const Vector3& vec) {
+    out << "{" << vec.getP1() << "; " << vec.getP2() << "; " << vec.getP3() << "}";
+  }
 
-  virtual void printParticle(std::ostream& out, const Particle& par);
+  virtual void printParticle(std::ostream& out, const Particle& par) {
+    out << std::fixed << std::setprecision(5);
+    out << "Velocity: ";
+    printVector3(out, par.getVelocity());
+    out << " Coordinates: ";
+    printVector3(out, par.getCoordinates());
+    out << " Mass: " << par.getMass() << " Isospin: " << par.getI();
+  }
 
-  virtual void printSystemState(std::ostream& out, const Dynamic& dyn);
+  virtual void printSystemState(std::ostream& out, const Dynamic& dyn) {
+    if (dyn.getForce() != Vector3(0, 0, 0)) {
+      out << "Force: ";
+      printVector3(out, dyn.getForce());
+      out << std::endl;
+    }
+    out << "Particles:" << std::endl;
+    for (auto particle : dyn.getParticles()) {
+      printParticle(out, particle);
+      out << std::endl;
+    }
+  }
 };
