7204f4fa660633c7c68f5a19dac5653525e714ac
Тимур Ахметзянов <ahmetzianov.temur@ya.ru>
2024-01-06 20:07:29 +0500

    removed definitions from Write.h

--- include/Write.h
+++ include/Write.h
@@ -9,29 +9,9 @@
 
 class Write {
  public:
-  virtual void printVector3(std::ostream& out, const Vector3& vec) {
-    out << "{" << vec.getP1() << "; " << vec.getP2() << "; " << vec.getP3() << "}";
-  }
+  virtual void printVector3(std::ostream& out, const Vector3& vec) = 0;
 
-  virtual void printParticle(std::ostream& out, const Particle& par) {
-    out << std::fixed << std::setprecision(3);
-    out << "Velocity: ";
-    printVector3(out, par.getVelocity());
-    out << " Coordinates: ";
-    printVector3(out, par.getCoordinates());
-    out << " Mass: " << par.getMass() << " Isospin: " << par.getI();
-  }
+  virtual void printParticle(std::ostream& out, const Particle& par) = 0;
 
-  virtual void printSystemState(std::ostream& out, const Dynamic& dyn) {
-    if (dyn.getForce() != Vector3(0, 0, 0)) {
-      out << "Force: ";
-      printVector3(out, dyn.getForce());
-      out << std::endl;
-    }
-    out << "Particles:" << std::endl;
-    for (auto particle : dyn.getParticles()) {
-      printParticle(out, particle);
-      out << std::endl;
-    }
-  }
+  virtual void printSystemState(std::ostream& out, const Dynamic& dyn) = 0;
 };
