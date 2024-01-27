55790ab17c66fa2b05614b6c1fe8b6584a0fd46f
Тимур Ахметзянов <ahmetzianov.temur@ya.ru>
2024-01-27 15:51:54 +0300

    clean Read

--- include/Read.h
+++ include/Read.h
@@ -9,16 +9,9 @@
 
 class Read {
  public:
-  virtual Vector3 readVector3(std::istream& in) = 0;
+  virtual Vector3 readVector3() = 0;
 
-  virtual Vector3 Vector3FromStdString(const std::string& string) = 0;
+  virtual Particle readParticle() = 0;
 
-  virtual Particle readParticle(std::istream& in) = 0;
-
-  virtual Particle ParticleFromStdString(const std::string& string) = 0;
-
-  virtual Dynamic readSystem(std::istream& in, bool read_force, int particles_cnt) = 0;
-
-  virtual Dynamic readSystem(std::istream& in, bool read_force, int particles_cnt,
-                             const std::function<Vector3(Particle, Particle)>& calculateForce) = 0;
+  virtual Dynamic readSystem(int particles_cnt) = 0;
 };
