1bb0b78e97512b9120de7b8f66d6010e036cbd3b
Тимур Ахметзянов <ahmetzianov.temur@ya.ru>
2024-01-27 16:06:11 +0300

    clear Dynamic.h

--- include/Dynamic.h
+++ include/Dynamic.h
@@ -4,30 +4,16 @@
 
 #pragma once
 
-#include "Particle.h"
+#include "SystemState.h"
+#include "Read.h"
+#include "Update.h"
+#include "Write.h"
 
 
 class Dynamic {
  public:
-  Dynamic() = default;
-  Dynamic(Vector3 force, const std::vector<Particle> &particles)
-          : force_(force), particles_(particles) {
-    calculateForce_ = [](Particle par1, Particle par2) { return Vector3(); };
-  }
-
-  void setCalculateForce(const std::function<Vector3(Particle, Particle)> &calculateForce) {
-    calculateForce_ = calculateForce;
-  }
-
-  const Vector3 &getForce() const {
-    return force_;
-  }
-
-  const std::function<Vector3(Particle, Particle)> &getCalculateForce() const {
-    return calculateForce_;
-  }
 
  protected:
-  Vector3 force_;
+  std::function<Vector3(Particle)> external_f_;
   std::function<Vector3(Particle, Particle)> calculateForce_;
 };
