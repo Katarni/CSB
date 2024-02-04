f4b3d8189cfffad9c877569429b213c6962252df
Тимур Ахметзянов <ahmetzianov.temur@ya.ru>
2024-02-04 19:55:49 +0300

    StandardWrite.h changed to StreamWrite.h

--- include/implem/Write/StreamWrite.h
+++ include/implem/Write/StreamWrite.h
@@ -3,22 +3,27 @@
 #include "../../VWrite.h"
 
 
-class StandardWrite: public VWrite {
+class StreamWrite: public VWrite {
  public:
+  StreamWrite(std::ostream& ostream): ostream_(ostream) {}
+
   void printVector3(const Vector3& vec) override {
-    std::cout << vec.getP1() << ' ' << vec.getP2() << ' ' << vec.getP3() << ' ';
+    ostream_ << vec.getP1() << ' ' << vec.getP2() << ' ' << vec.getP3() << ' ';
   }
 
   void printParticle(const Particle& par) override {
     printVector3(par.getVelocity());
     printVector3(par.getCoordinates());
-    std::cout << par.getMass() << ' ' << par.getI() << std::endl;
+    ostream_ << par.getMass() << ' ' << par.getI() << std::endl;
   }
 
   void printSystemState(const SystemState& state) override {
-    std::cout << std::fixed << std::setprecision(13);
+    ostream_ << std::fixed << std::setprecision(13);
     for (auto particle : state.getParticles()) {
       printParticle(particle);
     }
   }
+
+ private:
+  std::ostream ostream_;
 };
